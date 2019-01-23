6785cda97a48f3362063d3859adf4e5027b696ac
ainou <mirella.mih1006@gmail.com>
2019-01-22 23:07:19 -0200

    Removed unused validation.

--- src/services/userService.js
+++ src/services/userService.js
@@ -2,7 +2,7 @@ const JWT = require('jsonwebtoken');
 const Boom = require('boom');
 const Crypto = require("crypto");
 const Config = require('../config/config');
-const cipher = Crypto.createCipher(Config.encrypt_config.type, Config.encrypt_config.secret_key);
+const cipher = Crypto.createCipheriv('aes-256-cbc');
 
 const UserDAO = require('../DAO/userDAO');
 
@@ -27,36 +27,27 @@ class UserService {
   }
 
   async register(res, data) {
-    if (data.length > 0){
-      const exists = this.user.get({ email: data.email });
-      if (!exists.length) {
-        return {
-          response: await this.user.post(data)
-            .then(value => {
-              const response = JSON.parse(JSON.stringify(value));
-              delete response.password;
-              return response;
-            })
-            .catch(error => { Boom.internal(error) }),
-          "TokenLogin": utils.createToken(data.email)
-        }
-      } else {
-        return Boom.conflict('Email já cadastrado');
+    const exists = this.user.get({ email: data.email });
+    if (!exists.length) {
+      return {
+        response: await this.user.post(data)
+          .then(value => {
+            const response = JSON.parse(JSON.stringify(value));
+            delete response.password;
+            return response;
+          })
+          .catch(error => { Boom.internal(error) }),
+        "TokenLogin": utils.createToken(data.email)
       }
     } else {
-      return Boom.notFound('Dados vazios ou não encontrados');
+      return Boom.conflict('Email já cadastrado');
     }
   };
 
   login(data) {
-    if (!data.length) {
-      return Boom.notFound('Dados vazios ou não encontrados');
-
-    } else {
-      const exists = this.user.get({email: data.email});
-      if (!exists) {
-        return Boom.unauthorized('Email não cadastrado')
-      }
+    const exists = this.user.get({email: data.email});
+    if (!exists) {
+      return Boom.unauthorized('Email não cadastrado')
     }
   }
 }
