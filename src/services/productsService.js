ba76e6b69434837b885e3cdf76958105883c2f4e
ainou <mirella.mih1006@gmail.com>
2019-01-26 22:08:54 -0200

    Created new validation for token for some endpoints.

--- src/services/productsService.js
+++ src/services/productsService.js
@@ -21,12 +21,36 @@ class ProductsService {
 
   async getProducts(req) {
     await Middleware.validateToken(req);
-    const products = this.products.get({_id: req.params.id});
+    const products = await this.products.get({_id: req.params.id});
     if (!products) {
       throw Boom.notFound('Produto não encontrado!');
     } else {
       return products;
     }
+  };
+
+  async getAll(req) {
+    const products = await this.products.list({}, req.query.limit, req.query.ignore);
+    if (!products || products.length < 0) {
+      throw Boom.notFound('Nenhum produto cadastrado')
+    } else {
+      return products;
+    }
+  };
+
+  async postProducts(req) {
+    await Middleware.validateToken(req);
+    return await this.products.post(req.body);
+  }
+
+  async putProducts(req) {
+    await Middleware.validateToken(req);
+    return await this.products.pull(req.body._id, req.body);
+  }
+
+  async deleteProducts(req) {
+    await Middleware.validateToken(req);
+    return await this.products.delete({_id: req.body._id}, req.body);
   }
 }
 
